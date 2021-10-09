bc1c8a6303aaa665e56a3eb40012fa584f32eafe
Gatien Oudoire <naguite.ytb@gmail.com>
2021-10-09 10:29:16 +0200

    maj workflow

---
+++ setup.py
@@ -0,0 +1,4 @@
+from distutils.core import setup
+
+setup(name="frpython", version='1.0', description='Tentative de traduction de python en francais',
+      author="Gatien Oudoire", packages=["frpython"], url="https://pypi.org/project/frpython", download_url="https://pypi.org/project/frpython")
