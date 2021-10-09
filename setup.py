e2714a8d2543411e4e1aac3d0b64e34f7c097ce4
Gatien Oudoire <naguite.ytb@gmail.com>
2021-10-09 12:47:39 +0200

    1.1

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from distutils.core import setup
 
-setup(name="frpython", version='1.0', description='Tentative de traduction de python en francais',
-      author="Gatien Oudoire", packages=["frpython"], url="https://pypi.org/project/frpython", download_url="https://pypi.org/project/frpython")
+setup(name="frpython", version='1.1', description='Tentative de traduction de python en francais',
+      author="Gatien Oudoire", packages=["frpython"], url="https://pypi.org/project/frpython", download_url="https://pypi.org/project/frpython", py_modules=["chaines.py", "decimaux.py", "entiers.py", "erreurs.py"])
